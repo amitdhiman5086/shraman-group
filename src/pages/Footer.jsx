a9ee4cb1d9af06d5da494b63c711793686fc5b4d
Amit Dhiman <amitdhiman5086@gmail.com>
2024-08-22 17:34:21 +0530

    I am

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../utils/logo.png";
 import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+
+import { CiLinkedin } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div className="w-full h-[450px] bg-red-500 grid grid-cols-10">
-      <div className="bg-blue-600 col-span-4 flex justify-center mt-11">
-        <div className="bg-white w-1/2 h-auto ">
+    <div className="flex flex-col">
+        <div className="w-full h-[450px]  grid grid-cols-10">
+      <div className=" col-span-4 flex justify-center mt-11">
+        <div className=" w-1/2 h-auto ">
           <img src={logo} className="h-7  mb-3" alt="logo" />
           <p className="mt-10 font-normal ">
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -14,8 +18,8 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="bg-green-500 col-span-2 flex justify-center  mt-11">
-        <div className="bg-white w-1/2 h-auto ">
+      <div className=" col-span-2 flex justify-center  mt-11">
+        <div className=" w-1/2 h-auto ">
           <span className="font-bold">Links</span>
           <ul className=" mt-10 flex flex-col gap-3 font-extralight ">
             <Link to={"/"}>
@@ -44,10 +48,10 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="bg-green-100 col-span-4 flex justify-start  mt-11">
-        <div className="bg-white w-1/2 h-auto">
+      <div className=" col-span-4 flex justify-start  mt-11">
+        <div className=" w-1/2 h-auto">
           <span className="font-bold ">Contact Us</span>
-          <div className="flex  justify-between h-[80%] flex-col">
+          <div className="flex  justify-between h-[60%] flex-col">
             <div className="mt-10 flex flex-col gap-3 font-extralight">
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -60,16 +64,31 @@ const Footer = () => {
                 +923183561921
               </Link>
             </div>
-            <div className="bg-green-500">
-                <ul>
-                    <li>
-                        
-                    </li>
-                </ul>
+            <div className="">
+              <ul className="flex  gap-2 justify-end mr-4">
+                <Link to={"#"}>
+                  <FaFacebookF className="size-8 rounded-full m-1  p-[2px] shadow-md" />
+                </Link>
+                <Link to={"#"}>
+                  <FaInstagram className="size-8 rounded-full m-1  p-[2px] shadow-md" />
+                </Link>
+                <Link to={"#"}>
+                  <CiLinkedin className="size-8 rounded-full m-1  p-[2px] shadow-md" />
+                </Link>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    <div className="flex flex-col">
+        <div className="w-full bg-gray-500 h-[2px]">
+             
+        </div>
+        <div className="text-center p-3 font-light">
+        © 2024 Copyright by IK Developers. All rights reserved.
+        </div>
+    </div>
     </div>
   );
 };

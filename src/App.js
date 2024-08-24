803e4639d65bbfa8a545e3d180f6d5dfafa513eb
Amit Dhiman <amitdhiman5086@gmail.com>
2024-08-24 10:37:03 +0530

    Little Change

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Navbar from "./pages/Navbar";
 function App() {
   return (
     <div className="flex flex-col h-screen justify-between">
-      <div className="sticky top-0 bg-white">
+      <div className="sticky top-0 z-10 bg-white">
         <Navbar />
       </div>
-      <div className="mb-auto">
+      <div className="mb-auto z-0">
         {" "}
         <Outlet />
       </div>
